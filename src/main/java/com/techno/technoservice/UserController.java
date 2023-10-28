8909f8acb12258d1a2ae0497fa1634bb2249ff20
riddhish <riddhish.k@somaiya.edu>
2023-10-28 17:21:27 +0530

    WorkingApiWithCors

--- src/main/java/com/techno/technoservice/UserController.java
+++ src/main/java/com/techno/technoservice/UserController.java
@@ -1,12 +1,16 @@
 package com.techno.technoservice;
 
 import com.techno.technoservice.entity.LoginInput;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
-@CrossOrigin
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -15,5 +19,19 @@ public class UserController {
     @PostMapping(value = "sign-up")
     public Boolean signUp(@RequestBody LoginInput loginInput){
         return userService.signup(loginInput.getUserEmail(),loginInput.getPassword());
+    }
+
+    @PostMapping(consumes = "application/json")
+    public String login(@RequestBody Map<String,String> map){
+        log.info("In Controller!");
+        log.info(map.toString());
+        return "Hi there";
+    }
+
+    @GetMapping
+    public Map<String,String> getRiddhish(){
+        HashMap<String,String> map=new HashMap<>();
+        map.put("Riddhish","khot");
+        return map;
     }
 }
